24640e17247e1e4aa0c5a060d3c42c8b4040fbde
Yessica Romero <124018374+YessicaRomero@users.noreply.github.com>
2023-04-26 20:37:28 -0300

    initial commit

---
+++ ConFuncion.ts
@@ -0,0 +1,25 @@
+import * as rls from "readline-sync";
+
+
+
+let dimension: number = rls.questionInt("ingrese la dimension: ");
+let numeros: number[] = new Array(dimension);
+let positivos: number = 0;
+let negativos: number = 0;
+let ceros: number = 0;
+
+function Numeros(arrNew:number[]):void{
+    for(let i: number = 0;i<=numeros.length - 1;i++){
+        numeros[i] = rls.questionInt("ingrese un numero: ")
+        if(numeros[i]>0){
+            positivos++;
+        } if(numeros[i]<0){
+            negativos++;
+        } if(numeros[i]===0){
+            ceros++;
+        }
+        
+    }
+    console.log("los numeros positivos son: ", positivos , "los negativos son: ", negativos , "la cantidad de ceros son: " , ceros)
+}
+Numeros(numeros);
